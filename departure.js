1afa57f4881102dd54920089d9e59087a34fe865
Lindsay Koprivica <lindsayeplin@gmail.com>
2019-01-22 13:01:13 +0800

    checkpoint for node-greeting-app complete

---
+++ departure.js
@@ -0,0 +1,16 @@
+
+exports.departureEnglish = function() {
+  console.log("Goodbye");
+}
+
+exports.departureJapanese = function() {
+  console.log("Sayonara")
+}
+
+exports.departureSpanish = function() {
+  console.log("Adios");
+}
+
+exports.departureIcelandic = function() {
+  console.log("bless");
+}
---
+++ main.js
@@ -0,0 +1,20 @@
+
+const greetings = require("./greeting.js");
+
+const departures = require("./departure.js");
+
+greetings.greetingEnglish();
+
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+departures.departureEnglish();
+
+departures.departureSpanish();
+
+departures.departureJapanese();
+
+departures.departureIcelandic();
